62d1348ecbfee4c866aa05183dab0b429bc372a8
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-12-30 22:01:19 +0800

    Update ResourceEmulator.cpp

--- CSOPESY_Courseware-Complete/ResourceEmulator.cpp
+++ CSOPESY_Courseware-Complete/ResourceEmulator.cpp
@@ -1,5 +1,21 @@
 #include "ResourceEmulator.h"
 
+ResourceEmulator* ResourceEmulator::sharedInstance = nullptr;
+ResourceEmulator* ResourceEmulator::getInstance()
+{
+	return sharedInstance;
+}
+
+void ResourceEmulator::initialize()
+{
+	sharedInstance = new ResourceEmulator();
+}
+
+void ResourceEmulator::destroy()
+{
+	delete sharedInstance;
+}
+
 CPUWorker::CPUWorker(std::shared_ptr<IExecutableAction> executableAction)
 {
 	this->executableAction = executableAction;
